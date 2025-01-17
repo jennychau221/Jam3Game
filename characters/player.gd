833bdbdb876ff995c73eb020c737aa690164ffe4
Linnncolnnn <74068923+Linnncolnnn@users.noreply.github.com>
2025-01-16 16:24:07 -0800

    Lighting update
    Added shadow casts to most lighting in game
    
    added volumetric fog and AO
    
    Fixed headbob camera placement bug
    
    Other tweaks and fixes :)

--- characters/player.gd
+++ characters/player.gd
@@ -3,6 +3,11 @@ extends CharacterBody3D
 #Player stats
 var SPEED = 4
 
+#bob variables
+const BOB_FREQ = 2.4
+const BOB_AMP = 0.08
+var t_bob = 0.0
+
 #Player camera References
 var look_dir: Vector2
 @onready var camera = $Camera3D
@@ -36,31 +41,26 @@ func _physics_process(delta):
 		velocity.y -= gravity * delta
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction && !inputting:
+	var direction = (transform.basis * Vector3(input_dir.x, 1, input_dir.y)).normalized()
+	var is_moving = input_dir.length() > 0
+
+	if is_moving && !inputting:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-		if !footSounds.is_playing():
+		if !footSounds.is_playing() && is_on_floor():
 			footSounds.play()
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-		if footSounds.is_playing():
-			footSounds.stop()
+		footSounds.stop()
 		
 	#Interact function: runs when the raycast is colliding with a properly layered object and "enter" is pressed
-	#not runnable when player is inputting something to avoid clearing text
 	if  aim.is_colliding() && Input.is_action_just_pressed("interact") && !inputting:
 		lookedObject = aim.get_collider()
-		#This line here draws the text to be displayed from a metadata variable added in the static body of the looked at object
-		#This must be a string named interactText and the static body must be on collision layer 2 to avoid null values
 		descUI.text = "" + lookedObject.get_meta("interactText")
 		lookingAt()
 	
-	#"Pause" function, pressing "escape" will make mouse unhidden and unlocked, also checks first to see if text UI
-	#Is up and removes it first if it is.
 	if Input.is_action_just_pressed("pause"):
 		if descUI.visible == true:
 			descUI.visible = false
@@ -70,16 +70,24 @@ func _physics_process(delta):
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 		else: Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		
-	#This submits the text field with checks to make sure it happens only when meant too.
 	if Input.is_action_just_pressed("input") && inputting: 
-		#This signal gets the object ID from metadata and then the inputted text and sends it to the main scene code
 		passInput.emit(lookedObject.get_meta("objectID"), inputUI.text) 
 		inputUI.visible = false
 		inputting = false
-		#descUI.visible = false
 	
 	_rotate_camera(delta)
 	move_and_slide()
+
+	# Head bob - now tied to input rather than velocity
+	if is_moving && is_on_floor():
+		t_bob += delta * BOB_FREQ
+	camera.transform.origin = _headbob(t_bob)
+	
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * BOB_FREQ) * BOB_AMP + 0.6
+	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
+	return pos
 
 func _input(event: InputEvent):
 	if event is InputEventMouseMotion: look_dir = event.relative * 0.01
@@ -91,16 +99,13 @@ func _rotate_camera(delta: float, sens_mod: float = 1.0):
 	camera.rotation.x = clamp(camera.rotation.x - look_dir.y * camera_sens * sens_mod * delta, -1.5, 1.5)
 	look_dir = Vector2.ZERO
 
-#This function manages code for various effects when interacting with certain objects.
 func lookingAt():
 	descUI.visible = true
-	#Layer three input controls for text input puzzles
 	if lookedObject.get_collision_mask_value(3) == true:
-		inputUI.clear() #This clears all previous text stored in the Line Edit node
-		inputUI.visible = true #This makes the input field visible
-		inputUI.grab_focus() #This grabs focus for the input field
-		inputting = true #This sets a flag to disable traditional movement
-	#
+		inputUI.clear()
+		inputUI.visible = true
+		inputUI.grab_focus()
+		inputting = true
 	if lookedObject.get_collision_mask_value(4) == true:
 		itemCheck.emit(lookedObject.get_meta("objectID"))
 
@@ -109,22 +114,18 @@ func _on_main_solved_puzzle():
 	inputUI.visible = false
 	inputting = false
 
-
 func _on_area_3d_body_entered(body):
 	descUI.text = "Press E to go to up the stairs."
 	descUI.visible = true
 
-
 func _on_main_not_blind():
 	$UIControl/Blind.visible = false
-
 
 func _on_main_button_select():
 	$"UIControl/Stay Button".visible = true
 	$"UIControl/Stay Button".disabled = false
 	$UIControl/LeaveButton.visible = true
 	$UIControl/LeaveButton.disabled = false
-
 
 func _on_leave_button_pressed():
 	$"../AudioFiles/Endings/leaveEnding".play()
@@ -134,7 +135,6 @@ func _on_leave_button_pressed():
 	$UIControl/LeaveButton.disabled = true
 	await get_tree().create_timer(44).timeout
 	get_tree().change_scene_to_file("res://title.tscn")
-
 
 func _on_stay_button_pressed():
 	$"../AudioFiles/Endings/stayEnding".play()
